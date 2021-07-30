8785626ac228691aa2bd39da4d91f525a80d3ea0
kyhuy <kyhuytram@gmail.com>
2021-07-30 16:02:50 +0700

    get user list

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,8 @@ import axios from "../axios";
 const handleLoginApi = (userEmail, userPassword) => {
   return axios.post("/api/login", { email: userEmail, password: userPassword });
 };
-export { handleLoginApi };
+const getAllusers = (inputId) => {
+  //template String
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+export { handleLoginApi, getAllusers };
