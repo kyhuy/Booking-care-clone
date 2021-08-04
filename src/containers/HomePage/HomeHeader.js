b12d4eba756a76091a0412758dcdb4ef936d70ef
kyhuy <kyhuytram@gmail.com>
2021-08-04 10:32:50 +0700

    HomePage

---
+++ src/containers/HomePage/HomeHeader.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import "./HomeHeader.scss";
+class HomeHeader extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="home-header-container">
+          <div className="home-header-content">
+            <div className="left-content">
+              <i className="fas fa-bars"></i>
+              <div className="header-logo"></div>
+            </div>
+            <div className="center-content">
+              <div className="child-content">
+                <div>
+                  <b>Chuyên Khoa</b>
+                </div>
+                <div className="subs-title">Tìm bác sĩ theo chuyên khoa</div>
+              </div>
+              <div className="child-content">
+                <div>
+                  <b>Cơ sở Y tế</b>
+                </div>
+                <div className="subs-title">Chọn bệnh viện phòng khám</div>
+              </div>
+              <div className="child-content">
+                <div>
+                  <b>Bác Sĩ</b>
+                </div>
+                <div className="subs-title">Chọn bác sĩ giỏi</div>
+              </div>
+              <div className="child-content">
+                <div>
+                  <b>Gói Khám</b>
+                </div>
+                <div className="subs-title">Khám sức khỏe tổng quát</div>
+              </div>
+            </div>
+            <div className="right-content">
+              <div className="support">
+                <i className="fas fa-question-circle"></i>Hỗ trợ
+              </div>
+              <div className="flag">VN</div>
+            </div>
+          </div>
+        </div>
+        <div className="home-header-banner">
+          <div className="content-up">
+            <div className="title1">NỀN TẢNG Y TẾ</div>
+            <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+            <div className="search">
+              <i class="fas fa-search"></i>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="content-down">
+            <div className="options">
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám từ xa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám tổng quát</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Xét nghiệm Y học</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Sức khỏe tinh thần</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám nha khoa</div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child">
+                  <i class="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">Khám chuyên khoa</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
