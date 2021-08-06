31e78d936dad52c470ebb80fc71e95956bfd050f
kyhuy <kyhuytram@gmail.com>
2021-08-06 21:36:32 +0700

    css carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -8,38 +8,58 @@ import { FormattedMessage } from "react-intl";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import specialtyImg from "../../../assets/specialty/co-xuong-khop.jpg";
+
 class Specialty extends Component {
   render() {
     let settings = {
       dots: false,
       infinity: true,
       speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      // nextArrow: <SampleNextArrow />,
+      // prevArrow: <SamplePrevArrow />,
     };
     return (
       <div className="section-specialty">
-        <div className="specialty-content">
-          <Slider {...settings}>
-            <div className="img-customize">
-              <h3>1</h3>
-            </div>
-            <div className="img-customize">
-              <h3>2</h3>
-            </div>
-            <div className="img-customize">
-              <h3>3</h3>
-            </div>
-            <div className="img-customize">
-              <h3>4</h3>
-            </div>
-            <div className="img-customize">
-              <h3>5</h3>
-            </div>
-            <div className="img-customize">
-              <h3>6</h3>
-            </div>
-          </Slider>
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span className="title-section">Chuyên khoa phổ biên</span>
+            <button className="btn-section">Xem thêm</button>
+          </div>
+          <div className="specialty-body">
+            <Slider {...settings}>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 1</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 2</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 3</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 4</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 5</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 6</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Khoa châm cứu 7</div>
+              </div>
+            </Slider>
+          </div>
         </div>
       </div>
     );
