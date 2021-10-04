53439ed2083b0aaa958a8326ce9203b7d0d3d05d
kyhuy <kyhuytram@gmail.com>
2021-10-04 21:25:57 +0700

    video 68

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -11,12 +11,8 @@ import MdEditor from "react-markdown-editor-lite";
 import "react-markdown-editor-lite/lib/index.css";
 //================================================
 import Select from "react-select";
+import { LANGUAGES } from "../../../utils";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
 //==================================================
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
@@ -29,10 +25,42 @@ class ManageDoctor extends Component {
       contentHTML: "",
       selectedOptions: "",
       description: "",
+      listDoctors: [],
     };
   }
-  componentDidMount() {}
-  componentDidUpdate(prevProps, prevState, snapshot) {}
+  componentDidMount() {
+    this.props.fetchAllDoctors();
+  }
+  buildDataInputSelect = (inputData) => {
+    let result = [];
+    let { language } = this.props;
+
+    if (inputData && inputData.length > 0) {
+      inputData.map((item, index) => {
+        let object = {};
+        let labelVi = `${item.lastName} ${item.firstName}`;
+        let labelEn = `${item.firstName} ${item.lastName}`;
+        object.label = language === LANGUAGES.VI ? labelVi : labelEn;
+        object.value = item.id;
+        result.push(object);
+      });
+    }
+    return result;
+  };
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.allDoctors !== this.props.allDoctors) {
+      let dataSelect = this.buildDataInputSelect(this.props.allDoctors);
+      this.setState({
+        listDoctors: dataSelect,
+      });
+    }
+    if (prevProps.language !== this.props.language) {
+      let dataSelect = this.buildDataInputSelect(this.props.allDoctors);
+      this.setState({
+        listDoctors: dataSelect,
+      });
+    }
+  }
   handleEditorChange = ({ html, text }) => {
     this.setState({
       contentMarkdown: text,
@@ -40,7 +68,12 @@ class ManageDoctor extends Component {
     });
   };
   handleContentMarkDown = () => {
-    console.log("hoi dan it check state: ", this.state);
+    this.props.saveDetailDoctor({
+      contentHTML: this.state.contentHTML,
+      contentMarkdown: this.state.contentMarkdown,
+      description: this.state.description,
+      doctorId: this.state.selectedOptions.value,
+    });
   };
   handleChange = (selectedOptions) => {
     //console.log("check options from manage Doctor : ", selectedOptions);
@@ -53,6 +86,7 @@ class ManageDoctor extends Component {
     });
   };
   render() {
+    console.log("check state from ManageDoctor : ", this.state);
     return (
       <div className="manage-doctor-container">
         <div className="manage-doctor-title">Tạo thêm thông tin doctor</div>
@@ -62,7 +96,7 @@ class ManageDoctor extends Component {
             <Select
               value={this.state.selectedOptions}
               onChange={this.handleChange}
-              options={options}
+              options={this.state.listDoctors}
             />
           </div>
           <div className="content-right ">
@@ -97,14 +131,15 @@ class ManageDoctor extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    listUsers: state.admin.users,
+    language: state.app.language,
+    allDoctors: state.admin.allDoctors,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchUserRedux: () => dispatch(actions.fetchAllUsersStart()),
-    deleteAUserRedux: (id) => dispatch(actions.deleteAUser(id)),
+    fetchAllDoctors: (id) => dispatch(actions.fetchAllDoctors()),
+    saveDetailDoctor: (data) => dispatch(actions.saveDetailDoctor(data)),
   };
 };
 
--- src/store/actions/adminAction.js
+++ src/store/actions/adminAction.js
@@ -6,6 +6,8 @@ import {
   deleteUserService,
   editUserService,
   getTopDoctorHomeService,
+  getAllDoctors,
+  saveDetailDoctorService,
 } from "../../services/userService";
 import { toast } from "react-toastify";
 // export const fetchGenderStart = () => ({
@@ -209,6 +211,54 @@ export const fetchTopDoctor = () => {
       console.log("FETCH_TOP_DOCTORS_FAILED : ", e);
       dispatch({
         type: actionTypes.FETCH_TOP_DOCTORS_FAILED,
+      });
+    }
+  };
+};
+export const fetchAllDoctors = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllDoctors();
+      if (res && res.errCode === 0) {
+        dispatch({
+          type: actionTypes.FETCH_ALL_DOCTORS_SUCCESS,
+          dataDr: res.data,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.FETCH_ALL_DOCTORS_FAILED,
+        });
+      }
+    } catch (e) {
+      console.log("FETCH_ALL_DOCTORS_FAILED : ", e);
+      dispatch({
+        type: actionTypes.FETCH_ALL_DOCTORS_FAILED,
+      });
+    }
+  };
+};
+
+export const saveDetailDoctor = (data) => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await saveDetailDoctorService(data);
+      if (res && res.errCode === 0) {
+        toast.success("Save info detail doctor succeed");
+        dispatch({
+          type: actionTypes.SAVE_DETAIL_DOCTOR_SUCCESS,
+        });
+      } else {
+        console.log(res);
+        toast.error("Save info detail doctor error");
+        dispatch({
+          type: actionTypes.SAVE_DETAIL_DOCTOR_FAILED,
+        });
+      }
+    } catch (e) {
+      toast.error("Save info detail doctor error");
+      console.log("SAVE_DETAIL_DOCTOR_FAILED : ", e);
+      dispatch({
+        type: actionTypes.SAVE_DETAIL_DOCTOR_FAILED,
       });
     }
   };
